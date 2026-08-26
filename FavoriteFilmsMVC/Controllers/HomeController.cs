2991d4ff3c0b6fde827cb36c42104bf2bbdefd60
ZemNyux <nikita.gaber2703@gmail.com>
2026-08-26 15:47:55 +0300

    Добавьте файлы проекта.

---
+++ FavoriteFilmsMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using FavoriteFilmsMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FavoriteFilmsMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var movies = await _context.Movies.ToListAsync();
+        return View(movies);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ FavoriteFilmsMVC/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using FavoriteFilmsMVC.Models;
+
+namespace FavoriteFilmsMVC;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+
+        app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<FavoriteFilmsMVC.Models.AppDbContext>();
+            db.Database.EnsureCreated();
+        }
+
+        app.Run();
+
+        app.Run();
+    }
+}
